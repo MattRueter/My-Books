e005fe51b42e8c315bbde1de6a3ce611d46b99ba
Robert Matthew Rueter <84277551+MattRueter@users.noreply.github.com>
2023-08-17 11:09:41 -0500

    Create add and delete routes and helper functions.

--- query_functions/queryCmds.mjs
+++ query_functions/queryCmds.mjs
@@ -18,11 +18,26 @@ async function queryDB_sort(query, currentCollection, sortCriteria){
     return result
 }
 
+// ADD a book
+async function queryDB_post (query, currentCollection){
+    const collection = await db.collection(currentCollection);
+    const result = await collection.insertOne(query);
+
+    return result;
+}
+
+async function queryDB_delete(query, currentCollection){
+    const collection = await db.collection(currentCollection);
+    const result = await collection.deleteOne(query);
+
+    return result;
+};
+
 //check new item isn´t already in DB.
-async function isDuplicate (query, currentCollection) {
+async function documentExists (query, currentCollection) {
     let collection = await db.collection(currentCollection);
     let result = await collection.find(query).toArray();
-  
+
     if(result.length > 0){
         return true
     }else{
@@ -30,4 +45,4 @@ async function isDuplicate (query, currentCollection) {
     }
 }
 
-export const queryType = { queryDB, queryDB_sort, isDuplicate }
+export const queryType = { queryDB, queryDB_sort, queryDB_post, queryDB_delete, documentExists }
--- routes/book_route.mjs
+++ routes/book_route.mjs
@@ -1,11 +1,10 @@
 import express from "express";
-import db from "../db/conn.mjs";
 import { queryType } from "../query_functions/queryCmds.mjs";
 import { utils } from "../utils/utility_functions.mjs"
 
 const bookRouter = express.Router();
 const currentCollection = "books";
-const { queryDB, queryDB_sort } = queryType;
+const { queryDB, queryDB_sort, queryDB_post, queryDB_delete, documentExists } = queryType;
 const { convertToBoolean } = utils;
 
 //READ: --------------------------------------------------------------------------------------------
@@ -15,7 +14,7 @@ bookRouter.get("/:sortby", async (req,res) => {
     const sort = { [req.params.sortby] : 1 }
     const query = {}
     const result = await queryDB_sort(query, currentCollection, sort);
-
+    console.log(result)
     res.send(result).status(200);
 
 });
@@ -31,5 +30,31 @@ bookRouter.get("/:filter/:criteria", async (req,res) =>{
     res.send(result).status(200);
 });
 
+//ADD a book.
+bookRouter.post("/addBook/:bookObject", async(req,res) => {
+    const newBook = JSON.parse(req.params.bookObject)
+    const alreadyExists = await documentExists(newBook,currentCollection);
+
+
+    if(alreadyExists){
+        res.status(409).send("Already exists")
+    }else{
+        const result = await queryDB_post(newBook, currentCollection)
+        res.status(201).send(result);
+    }
+});
+
+bookRouter.delete("/deleteBook/:bookTitle", async(req,res) =>{
+    const book = { title : req.params.bookTitle };
+    console.log(book)
+    const exists = await documentExists(book,currentCollection);
+    console.log(exists)
+    if(exists){
+        const result = await queryDB_delete(book,currentCollection);
+        res.status(200).send(result)
+    }else{
+        res.status(404).send("book not found.")
+    }
+});
 
 export default bookRouter;
