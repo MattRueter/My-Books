aa1c5b1eedd6189bae8a2b3f2d4dcb3d71dc4308
Robert Matthew Rueter <84277551+MattRueter@users.noreply.github.com>
2023-08-22 12:54:55 -0500

    add basic serverside pagination.

--- My_Books-UI/server.mjs
+++ My_Books-UI/server.mjs
@@ -5,30 +5,56 @@ import { paginationFunctions } from "./utils/pagination.mjs";
 
 const PORT = 3000;
 const app = express();
-const { getTotalPages } = paginationFunctions;
-
-//------------------------------------------------------------------------------------------------
-//MIDDLEWARE:
-app.set('view engine', 'pug');
-app.use(cors());
-app.use(express.json());
-app.use(express.static("./"));
-
-
-// ROUTES ----------------------------------------------------
-app.get("/", (req,res) =>{
-    //just sending the html files for now.
-    res.render("home", {books:[]})
-});
-
-
-
-app.get("/getAllBooks/:bySortValue", async (req,res) =>{
-    const query = req.query.sort
-    //const response = await fetch(`http://localhost:5000/book/${queryValue}`);
-    //const books = await response.json();
-    const books =[
-        {
+const { getTotalPages, createPages } = paginationFunctions;
+const itemsPerPage = 5 // could be sent from UI based on preferences?
+let currentBooks;
+let booksCopy;
+const books =[
+    {
+    author: "Italo Calvino",
+    author_firstName: "Italo",
+    author_lastName: "Calvino",
+    genres: 
+    (2) ['Italian Literature', 'Short Stories'],
+    have_read: true, 
+    language: "English",
+    title: "Baron in the Trees", 
+    _id: "64dd6e812448e1d7a4c7dd8b"
+    },
+    {
+    author: "Italo Calvino",
+    author_firstName: "Italo",
+    author_lastName: "Balvino",
+    genres: 
+    (2) ['Italian Literature', 'Short Stories'],
+    have_read: true, 
+    language: "English",
+    title: "Invisible Cities", 
+    _id: "64dd6e812448e1d7a4c7dd8b"
+    },
+    {
+    author: "Italo Calvino",
+    author_firstName: "Italo",
+    author_lastName: "Stalinalvino",
+    genres: 
+    (2) ['Italian Literature', 'Short Stories'],
+    have_read: true, 
+    language: "English",
+    title: "If on a Winter's Night a Traveler.", 
+    _id: "64dd6e812448e1d7a4c7dd8b"
+    },
+    {
+    author: "Italo Calvino",
+    author_firstName: "Italo",
+    author_lastName: "Malvino",
+    genres: 
+    (2) ['Italian Literature', 'Short Stories'],
+    have_read: true, 
+    language: "English",
+    title: "Adam, One Afternoon", 
+    _id: "64dd6e812448e1d7a4c7dd8b"
+    },
+    {
         author: "Italo Calvino",
         author_firstName: "Italo",
         author_lastName: "Calvino",
@@ -160,62 +186,68 @@ app.get("/getAllBooks/:bySortValue", async (req,res) =>{
                 title: "Adam, One Afternoon", 
                 _id: "64dd6e812448e1d7a4c7dd8b"
                 },
-                {
-                    author: "Italo Calvino",
-                    author_firstName: "Italo",
-                    author_lastName: "Calvino",
-                    genres: 
-                    (2) ['Italian Literature', 'Short Stories'],
-                    have_read: true, 
-                    language: "English",
-                    title: "Baron in the Trees", 
-                    _id: "64dd6e812448e1d7a4c7dd8b"
-                    },
-                    {
-                    author: "Italo Calvino",
-                    author_firstName: "Italo",
-                    author_lastName: "Balvino",
-                    genres: 
-                    (2) ['Italian Literature', 'Short Stories'],
-                    have_read: true, 
-                    language: "English",
-                    title: "Invisible Cities", 
-                    _id: "64dd6e812448e1d7a4c7dd8b"
-                    },
-                    {
-                    author: "Italo Calvino",
-                    author_firstName: "Italo",
-                    author_lastName: "Stalinalvino",
-                    genres: 
-                    (2) ['Italian Literature', 'Short Stories'],
-                    have_read: true, 
-                    language: "English",
-                    title: "If on a Winter's Night a Traveler.", 
-                    _id: "64dd6e812448e1d7a4c7dd8b"
-                    },
-                    {
-                    author: "Italo Calvino",
-                    author_firstName: "Italo",
-                    author_lastName: "Malvino",
-                    genres: 
-                    (2) ['Italian Literature', 'Short Stories'],
-                    have_read: true, 
-                    language: "English",
-                    title: "Adam, One Afternoon", 
-                    _id: "64dd6e812448e1d7a4c7dd8b"
-                    },
-    ]
+]
+
+//------------------------------------------------------------------------------------------------
+//MIDDLEWARE:
+app.set('view engine', 'pug');
+app.use(cors());
+app.use(express.json());
+app.use(express.static("./"));
+
+
+// ROUTES ----------------------------------------------------
+app.get("/", (req,res) =>{
+    //just sending the html files for now.
+    res.render("home", {books:[]})
+});
+
+
+
+app.get("/getAllBooks/:bySortValue", async (req,res) =>{
+    /*  1.Fetches books by sort criteria from DB.
+        2.Paginates that data and sends only the first page to the front end.
+        3.Assigns fetched data to global currentBooks. This is subsequently used on getPage routes avoids unecessary queries to DB.
+        4. Creates objcet used by client for creating corrrect number of pages buttons.
+    */
+
+    // Task #1
+    const query = req.query.sort
+    //const response = await fetch(`http://localhost:5000/book/${queryValue}`);
+    //const books = await response.json();
+    booksCopy = books.map(page => page);
+
+    // Task #2
+    const pages = createPages(getTotalPages(books,itemsPerPage), itemsPerPage, books);
+
+    // Task #3
+    currentBooks = pages.map(page => page);
     
-    //move pagination logic into fact funk or class which turns out needed object wherever it
-    // may be needed.
-    const itemsPerPage = 5
+    // Task #4
     const pagination = {totalPages:getTotalPages(books, itemsPerPage), perPage:itemsPerPage }
-
+    
     res.render("home",{
-        books: books,
-        pagination: pagination
+        books : pages[0],
+        pagination : pagination
     })
+    
+});
 
+app.get("/getPage/:page", (req,res) => {
+    /*
+    when a user clicks a page button on the UI
+    this route gets the request. :page (req.query.page) will get converted to type=nubmer
+    and used to send books : page[toNumber(req.query.page)]
+    pagination object sent as well so that the correct number of page buttons persist (re-render really).
+    */
+    // * Books and currentBooks may be problematic as global copies of fetched data due to potential size.
+    const currentPage = Number(req.query.page) -1 
+    const pagination = {totalPages:getTotalPages(booksCopy, itemsPerPage), perPage:itemsPerPage }
+    
+    res.render("home", {
+        books : currentBooks[currentPage],
+        pagination :pagination
+    })
 });
 
 
@@ -223,7 +255,7 @@ app.get("/getAllBooks/:bySortValue", async (req,res) =>{
 //--------------------------------------------------------------
 
 app.listen(PORT, () => {
-    console.log(`Client UI listening on port ${PORT}`);
+    console.log(`My Books Client is listening on port ${PORT}`);
     
 });
 
--- My_Books-UI/utils/pagination.mjs
+++ My_Books-UI/utils/pagination.mjs
@@ -1,3 +1,182 @@
+const books =[
+    {
+    author: "Italo Calvino",
+    author_firstName: "Italo",
+    author_lastName: "Calvino",
+    genres: 
+    (2) ['Italian Literature', 'Short Stories'],
+    have_read: true, 
+    language: "English",
+    title: "Baron in the Trees", 
+    _id: "64dd6e812448e1d7a4c7dd8b"
+    },
+    {
+    author: "Italo Calvino",
+    author_firstName: "Italo",
+    author_lastName: "Balvino",
+    genres: 
+    (2) ['Italian Literature', 'Short Stories'],
+    have_read: true, 
+    language: "English",
+    title: "Invisible Cities", 
+    _id: "64dd6e812448e1d7a4c7dd8b"
+    },
+    {
+    author: "Italo Calvino",
+    author_firstName: "Italo",
+    author_lastName: "Stalinalvino",
+    genres: 
+    (2) ['Italian Literature', 'Short Stories'],
+    have_read: true, 
+    language: "English",
+    title: "If on a Winter's Night a Traveler.", 
+    _id: "64dd6e812448e1d7a4c7dd8b"
+    },
+    {
+    author: "Italo Calvino",
+    author_firstName: "Italo",
+    author_lastName: "Malvino",
+    genres: 
+    (2) ['Italian Literature', 'Short Stories'],
+    have_read: true, 
+    language: "English",
+    title: "Adam, One Afternoon", 
+    _id: "64dd6e812448e1d7a4c7dd8b"
+    },
+    {
+        author: "Italo Calvino",
+        author_firstName: "Italo",
+        author_lastName: "Calvino",
+        genres: 
+        (2) ['Italian Literature', 'Short Stories'],
+        have_read: true, 
+        language: "English",
+        title: "Baron in the Trees", 
+        _id: "64dd6e812448e1d7a4c7dd8b"
+        },
+        {
+        author: "Italo Calvino",
+        author_firstName: "Italo",
+        author_lastName: "Balvino",
+        genres: 
+        (2) ['Italian Literature', 'Short Stories'],
+        have_read: true, 
+        language: "English",
+        title: "Invisible Cities", 
+        _id: "64dd6e812448e1d7a4c7dd8b"
+        },
+        {
+        author: "Italo Calvino",
+        author_firstName: "Italo",
+        author_lastName: "Stalinalvino",
+        genres: 
+        (2) ['Italian Literature', 'Short Stories'],
+        have_read: true, 
+        language: "English",
+        title: "If on a Winter's Night a Traveler.", 
+        _id: "64dd6e812448e1d7a4c7dd8b"
+        },
+        {
+        author: "Italo Calvino",
+        author_firstName: "Italo",
+        author_lastName: "Malvino",
+        genres: 
+        (2) ['Italian Literature', 'Short Stories'],
+        have_read: true, 
+        language: "English",
+        title: "Adam, One Afternoon", 
+        _id: "64dd6e812448e1d7a4c7dd8b"
+        },
+        {
+            author: "Italo Calvino",
+            author_firstName: "Italo",
+            author_lastName: "Calvino",
+            genres: 
+            (2) ['Italian Literature', 'Short Stories'],
+            have_read: true, 
+            language: "English",
+            title: "Baron in the Trees", 
+            _id: "64dd6e812448e1d7a4c7dd8b"
+            },
+            {
+            author: "Italo Calvino",
+            author_firstName: "Italo",
+            author_lastName: "Balvino",
+            genres: 
+            (2) ['Italian Literature', 'Short Stories'],
+            have_read: true, 
+            language: "English",
+            title: "Invisible Cities", 
+            _id: "64dd6e812448e1d7a4c7dd8b"
+            },
+            {
+            author: "Italo Calvino",
+            author_firstName: "Italo",
+            author_lastName: "Stalinalvino",
+            genres: 
+            (2) ['Italian Literature', 'Short Stories'],
+            have_read: true, 
+            language: "English",
+            title: "If on a Winter's Night a Traveler.", 
+            _id: "64dd6e812448e1d7a4c7dd8b"
+            },
+            {
+            author: "Italo Calvino",
+            author_firstName: "Italo",
+            author_lastName: "Malvino",
+            genres: 
+            (2) ['Italian Literature', 'Short Stories'],
+            have_read: true, 
+            language: "English",
+            title: "Adam, One Afternoon", 
+            _id: "64dd6e812448e1d7a4c7dd8b"
+            },
+            {
+                author: "Italo Calvino",
+                author_firstName: "Italo",
+                author_lastName: "Calvino",
+                genres: 
+                (2) ['Italian Literature', 'Short Stories'],
+                have_read: true, 
+                language: "English",
+                title: "Baron in the Trees", 
+                _id: "64dd6e812448e1d7a4c7dd8b"
+                },
+                {
+                author: "Italo Calvino",
+                author_firstName: "Italo",
+                author_lastName: "Balvino",
+                genres: 
+                (2) ['Italian Literature', 'Short Stories'],
+                have_read: true, 
+                language: "English",
+                title: "Invisible Cities", 
+                _id: "64dd6e812448e1d7a4c7dd8b"
+                },
+                {
+                author: "Italo Calvino",
+                author_firstName: "Italo",
+                author_lastName: "Stalinalvino",
+                genres: 
+                (2) ['Italian Literature', 'Short Stories'],
+                have_read: true, 
+                language: "English",
+                title: "If on a Winter's Night a Traveler.", 
+                _id: "64dd6e812448e1d7a4c7dd8b"
+                },
+                {
+                author: "Italo Calvino",
+                author_firstName: "Italo",
+                author_lastName: "Malvino",
+                genres: 
+                (2) ['Italian Literature', 'Short Stories'],
+                have_read: true, 
+                language: "English",
+                title: "Adam, One Afternoon", 
+                _id: "64dd6e812448e1d7a4c7dd8b"
+                },
+]
+
 
 function getTotalPages (results, itemsPerPage) {
     const size = results.length;
@@ -6,7 +185,35 @@ function getTotalPages (results, itemsPerPage) {
     return totalPages
 };
 
-//______________________
+
+function createPages (totalPages, itemsPerPage, array) {
+    let pages = [];
+    let page = [];
+    let bookIndex = 0;
+    const pushBooksToPage = () =>{
+        for(let i=1; i<=itemsPerPage; i ++){
+            page.push(array[bookIndex]);
+            bookIndex ++;
+            if(checkIfLastPage(array, bookIndex)){return}
+        } 
+    }
+    
+    for(let i = 1; i<=totalPages; i++){
+        pushBooksToPage();
+        pages.push(page);
+        page = [];
+    }
+
+    return pages;
+}
+
+
+function checkIfLastPage (array, currentIndex){
+    let isLastItem = false;
+    currentIndex > array.length - 1 ? isLastItem = true : isLastItem = false;
+    return isLastItem;
+};
+//__________________________________________________________________________
 
 
 function createCurrentPageMsg (currentPage, totalPages) {
@@ -27,10 +234,7 @@ function turnPage (pg) {
     createCurrentPageMsg(page, getTotalPages(data))
 };
 
-function checkIfLastPage (results, lastIndex){
-    let index;
-    lastIndex > results.length - 1 ? index = results.length-1 : index = lastIndex;
-    return index;
-};
 
-export const paginationFunctions = { getTotalPages } 
+
+
+export const paginationFunctions = { getTotalPages, createPages } 
